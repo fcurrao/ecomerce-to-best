4a259b9ecf84bb51f69f1d3e34ab1974d047140e
GQue - DEVELOPED FRONT END <fcurrao@gmail.com>
2022-08-27 12:41:10 -0300

    initial2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS !
+          Learn React JS !!!
         </a>
       </header>
     </div>
